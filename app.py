0058360c842a612b1bc20846225cf675e36488c6
nilsonmolina <nilson13@me.com>
2018-12-21 08:13:40 -0800

    fixed dumb mistake

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'Ch@llenge64'
+app.secret_key = 'my_secret'
 api = Api(app)
 
 # THIS IS NOW IN: run.py
