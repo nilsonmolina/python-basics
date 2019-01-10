62f21dead7f22c3a61c03ba975850d001f2cf8e2
nilsonmolina <nilson13@me.com>
2019-01-10 12:20:55 -0800

    finished jwt-extended

--- flask-basics/08-jwt-extended/code/app.py
+++ flask-basics/08-jwt-extended/code/app.py
@@ -8,7 +8,7 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -36,6 +36,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
 
 # --------- START SERVER ---------
 if __name__ == "__main__":
